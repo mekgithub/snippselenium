be6f2e795c0dce2429f64a2cd0a093d79b4f5de9
mekgithub <mkortak1970@gmail.com>
2020-04-22 15:11:14 -0500

    1 th commit

---
+++ src/test/java/com/jenkinspro/tests/SnippingSelenium.java
@@ -0,0 +1,22 @@
+package com.jenkinspro.tests;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SnippingSelenium {
+    public static void main(String[] args) throws IOException {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\18327\\Downloads\\chromedriver_win32_ version80\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://www.amazon.com");
+        File file=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file,new File("C:\\Users\\18327\\Desktop\\myScreenShot2.bmp"));
+
+
+    }
+}
